73c02004a6ad75554e3e0defcf9d4faceea0afc7
dambor <glenioborges@gmail.com>
2025-04-07 21:11:01 -0400

    Initial commit of Borges React app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      {/* Your React components go here. This project uses index.html directly for now. */}
+      <h1>Welcome to Borges React App</h1>
+      <p>This is a placeholder React component.</p>
+    </div>
+  );
+}
+
+export default App;
